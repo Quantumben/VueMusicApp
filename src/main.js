2c9866050cf837142cbe4d6426ad5c85a4dba62f
QuantumBen <omosabenedict@gmail.com>
2023-04-02 05:27:10 +0100

    Show us the location of Pinia and Router

--- src/main.js
+++ src/main.js
@@ -10,6 +10,12 @@ import './assets/main.css'
 
 const app = createApp(App)
 
+/*
+This is where Pinia and router plugin 
+is been registered.
+It is done by using the function "use()"
+*/
+
 app.use(createPinia())
 app.use(router)
 
