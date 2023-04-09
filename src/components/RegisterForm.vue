6c55e931d963b2d38f92acd2fbdf81eed6b864b0
QuantumBen <omosabenedict@gmail.com>
2023-04-09 18:20:24 +0100

    Connect reg form to Google firebase

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -9,11 +9,7 @@
     {{ reg_alert_msg }}
   </div>
 
-  <vee-form
-     :validation-schema="schema"
-    @submit="register"
-    :initial-values="userData"
-  >
+  <vee-form :validation-schema="schema" @submit="register" :initial-values="userData">
     <!-- Name -->
     <div class="mb-3">
       <label class="inline-block mb-2">Name</label>
@@ -112,6 +108,8 @@
 </template>
 
 <script>
+import firebase from '@/includes/firebase'
+
 export default {
   name: 'RegisterForm',
 
@@ -139,17 +137,30 @@ export default {
     }
   },
   methods: {
-    register(values) {
+    async register(values) {
       this.reg_show_alert = true
       this.reg_in_submission = true
       this.reg_alert_variant = 'bg-blue-500'
       this.reg_alert_msg = 'Please wait! Your account is being created.'
 
-      this.reg_alert_variant = 'bg-green-500';
+      let userCred = null;
+
+      try {
+        userCred = await firebase
+          .auth()
+          .createUserWithEmailAndPassword(values.email, values.password)
+      } catch (error) {
+        this.reg_in_submission = false
+        this.reg_alert_variant = 'bg-red-500'
+        this.reg_alert_msg = 'An unexpected error occured. Please try again later.';
+        return;
+      }
+
+      this.reg_alert_variant = 'bg-green-500'
       this.reg_alert_msg = 'Success! Your account has been created.'
 
-      console.log(values) 
-    },
+      console.log(userCred)
+    }
   }
 }
 </script>
